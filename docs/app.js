24f83553cf651819b12ce3d404476b3abf4c3d1b
aquari412 <aq0412@outlook.jp>
2024-06-05 13:53:26 +0900

    コメントアウト

--- docs/app.js
+++ docs/app.js
@@ -1,23 +1,23 @@
-const videos =  document.querySelectorAll('.video');
+const videos = document.querySelectorAll('.video');
 
 console.log("aaaa");
 videos.forEach((video) => {
-    video.addEventListener('mouseover', () => video.play());
-    video.addEventListener('mouseout', () => video.pause());
+	video.addEventListener('mouseover', () => video.play()); //カーソルを乗せたとき動画再生
+	video.addEventListener('mouseout', () => video.pause()); //カーソルを離したとき再生停止
 })
 
-$(function() {
-	setTimeout(function(){
-		$('.logo_fadein p').fadeIn(1000);
-	},500);
-	setTimeout(function(){
-		$('.logo_fadein').fadeOut(800);
-	},2500);
+$(function () {
+	setTimeout(function () {
+		$('.logo_fadein p').fadeIn(1000); //ロゴのフェードイン
+	}, 500);
+	setTimeout(function () {
+		$('.logo_fadein').fadeOut(800); //ロゴのフェードアウト
+	}, 2500);
 });
 
-$(function() {
-	setTimeout(function(){
-		$('.main').fadeIn(1000);
-	},500);
+$(function () {
+	setTimeout(function () {
+		$('.main').fadeIn(1000); //メイン画面のフェードイン
+	}, 500);
 });
 
